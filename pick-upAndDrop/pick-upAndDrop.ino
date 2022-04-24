05bcad45229f2e49e0e1861c3c59699bf72f99db
PyromancerBoom <priyanshmagic@gmail.com>
2022-04-24 15:48:08 +0530

    code to pick up and drop

--- pick-upAndDrop/pick-upAndDrop.ino
+++ pick-upAndDrop/pick-upAndDrop.ino
@@ -2,9 +2,9 @@
 
 AF_DCMotor base(1), motor2(2), motor3(3), gripper(4);
 
-int common = 100;
-int delay_speed = 900;
-int m1_speed = common, m2_speed = common, m3_speed = 200, m4_speed = 100;
+int common = 90;
+int delay_speed = 2000;
+int m1_speed = 105, m2_speed = common, m3_speed = 200, m4_speed = 220;
 
 void setup()
 {
@@ -12,29 +12,28 @@ void setup()
   motor2.setSpeed(m2_speed);  // Right side -> BACK AND FORTH
   motor3.setSpeed(m3_speed);  // LEFT MOTOR -> UP AND DOWN
   gripper.setSpeed(m4_speed); // Gripper
-  
-  gripper.run(FORWARD);
-  delay(delay_speed);
-}
-
-void loop()
-{
-  base.run(FORWARD);
-  delay(delay_speed);
-
-  base.run(BACKWARD);
-  delay(delay_speed);
-
-  motor2.run(FORWARD);
-  delay(delay_speed);
 
   motor2.run(BACKWARD);
-  delay(delay_speed);
-
   motor3.run(FORWARD);
   delay(delay_speed);
 
-  motor3.run(BACKWARD);
+  gripper.run(BACKWARD);
   delay(delay_speed);
+
+  motor2.run(FORWARD);
+  motor3.run(BACKWARD);
+  delay(delay_speed*2);
   
+  base.run(FORWARD);
+  delay(delay_speed);
+
+  gripper.run(FORWARD);
+  delay(delay_speed*0.5);
+  
+  base.run(RELEASE);
+  gripper.run(RELEASE);
+  motor2.run(RELEASE);
+  motor3.run(RELEASE);
 }
+
+void loop(){}
